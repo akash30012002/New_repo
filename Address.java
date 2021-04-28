3849c22fe754dd35029ae05f9791e1a2034f050d
Akash <akash3001002@yahoo.co.in>
2021-04-28 20:23:45 +0530

    added all

---
+++ Address.java
@@ -0,0 +1,49 @@
+
+//public class Address {
+//
+//	private String street;
+//	private String city;
+//	private String state;
+//	
+//	Address(String street, String city, String state){
+//		this.street = street;
+//		this.city = city;
+//		this.state = state;
+//	}
+//	
+//	public String sendAddress() {
+//		return "Street: " + this.street + ", City: " + this.city + ", State: " + this.state;
+//	}
+//	
+//	public void updateAddress(String street, String city, String state) {
+//		this.street = street;
+//		this.city = city;
+//		this.state = state;
+//	}
+//
+//	public String getStreet() {
+//		return this.street;
+//	}
+//
+//	public void setStreet(String street) {
+//		this.street = street;
+//	}
+//
+//	public String getCity() {
+//		return this.city;
+//	}
+//
+//	public void setCity(String city) {
+//		this.city = city;
+//	}
+//
+//	public String getState() {
+//		return this.state;
+//	}
+//
+//	public void setState(String state) {
+//		this.state = state;
+//	}
+//	
+//	
+//}
---
+++ Contact.java
@@ -0,0 +1,49 @@
+//
+//public class Contact {
+//
+//	private String name;
+//	private String phoneNumber;
+//	private String email;
+//	
+//	Contact(String name, String phoneNumber, String email){
+//		this.name = name;
+//		this.phoneNumber = phoneNumber;
+//		this.email = email;
+//	}
+//	
+//	public String sendContact() {
+//		return "Name: " + this.name + ", Phone: " + this.phoneNumber + ", Email: " + this.email;
+//	}
+//	
+//	public void updateContact(String name, String phoneNumber, String email) {
+//		this.name = name;
+//		this.phoneNumber = phoneNumber;
+//		this.email = email;
+//	}
+//
+//	public String getName() {
+//		return this.name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getPhoneNumber() {
+//		return this.phoneNumber;
+//	}
+//
+//	public void setPhoneNumber(String phoneNumber) {
+//		this.phoneNumber = phoneNumber;
+//	}
+//
+//	public String getEmail() {
+//		return this.email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//	
+//	
+//}
